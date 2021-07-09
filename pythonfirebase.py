e0e2d1f0e51289117b930b9e51c451462379704a
trihims <himanshu.16bcs1156@abes.ac.in>
2021-07-09 21:22:58 +0530

    9/7/2021

--- pythonfirebase.py
+++ pythonfirebase.py
@@ -1,17 +1,36 @@
 import firebase_admin
+import json
 from firebase_admin import credentials
 from firebase_admin import db
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# Fetch the service account key JSON file contents
 cred = credentials.Certificate('serviceAccountKey.json')
 
-# Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://chatbotapp-330bf.firebaseio.com/'
 })
 
-# As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('sentchat')
-data = ref.get()
+english_bot = ChatBot("Chatterbot", storage_adapter= 'chatterbot.storage.SQLStorageAdapter')
+trainer = ChatterBotCorpusTrainer(english_bot.storage)
+trainer.train("chatterbot.corpus.english")
 
-print(data)
+ref = db.reference('sentchat')
+data = ref.get() 
+
+temp = list(data.values())
+temp2=dict(temp[-1])
+temp2m = temp2['message']
+
+name = temp2['user']
+
+request = temp2m
+response = english_bot.get_response(request)
+responseN = str(response)
+
+userName = 'CHATBOT'
+data = ref.push({
+              "user": userName,
+              "message": responseN
+          })
+
